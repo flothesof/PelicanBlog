5d078a872e807d6b2be0cb9013573c3939489e47
Florian Le Bourdais <florian.s.lebourdais@gmail.com>
2016-12-06 20:01:34 +0100

    tweaked theme: full summary on index page

--- pelicanconf.py
+++ pelicanconf.py
@@ -23,7 +23,7 @@ TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
-# activate feed summary (smaller feeds)
+# activate feed summary (smaller sized feeds)
 FEED_USE_SUMMARY = True	
 
 DEFAULT_PAGINATION = 10
